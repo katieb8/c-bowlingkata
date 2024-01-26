6fd521408b42a0a483cdb63794c0d4b80cc19ae4
Katie Burdett <Katie.Burdett@ao.com>
2024-01-26 08:55:56 +0000

    chore: add initial gutter and one pin tests

---
+++ BowlingKata/BowlingKata/Game.cs
@@ -0,0 +1,42 @@
+﻿
+using static System.Formats.Asn1.AsnWriter;
+
+namespace BowlingKata
+{
+    public class Game
+    {
+        //private List<int> _rolls = new List<int>();
+        private int[] _rolls = new int [21];
+        private int _currentRoll = 0;
+        private int _score;
+
+        public int Score()
+        {
+            _currentRoll = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                _score += _rolls[_currentRoll] + _rolls[_currentRoll + 1];
+                _currentRoll += 2;
+            }
+
+            //if (isSpare())
+            //{
+            //    _score += 10 + _rolls[_currentRoll + 2];
+            //    _score += 2;
+            //}
+
+            return _score;
+        }
+
+        //public bool isSpare()
+        //{
+        //    return _rolls[_currentRoll] + _rolls[_currentRoll + 1] == 10;
+        //}
+
+        public void Roll(int pinsKnocked)
+        {
+            _rolls[_currentRoll] += pinsKnocked;
+            _currentRoll++;
+        }
+    }
+}
---
+++ BowlingKata/BowlingKata/GameTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace BowlingKata
+{
+    [TestFixture]
+    public class GameTest
+    {
+        private Game _game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _game = new Game();
+        }
+        [Test]
+        public void GutterGame()
+        {
+            for (var i = 0; i < 20; i++)
+            {
+                _game.Roll(0);
+            }
+            Assert.That(_game.Score(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void RollTwoOnes()
+        {
+            _game.Roll(1);
+            _game.Roll(1);
+            Assert.That(_game.Score(), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void ScoreASpare()
+        {
+            RollASingleSpare();
+            Assert.That(_game.Score(), Is.EqualTo(12));
+        }
+
+        private void RollASingleSpare()
+        {
+            _game.Roll(1);
+            _game.Roll(9);
+
+            _game.Roll(1);
+        }
+    }
+}
